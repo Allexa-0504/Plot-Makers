724fd7fabd43dd47f28599fec079188b5641a93a
nugpasa.sgp.dom.my.id <lara-caroline@sgp.domcloud.co>
2024-12-27 18:27:02 +0000

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,145 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
+    <title>@yield('title', 'Plot Makers')</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700|Inconsolata:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('template/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/fonts/ionicons/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/fonts/fontawesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/fonts/flaticon/font/flaticon.css') }}">
+    <link rel="icon" type="image/png" href="{{ asset('template/images/logoTcc (1).png') }}">
+    <link rel="stylesheet" href="{{ asset('template/css/style.css') }}">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <div class="wrap">
+        <header role="banner">
+            <div class="top-bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-9 social">
+                            <a href="https://twitter.com/PlotMakers"><span class="fa fa-twitter"></span></a>
+                            <a href="https://www.instagram.com/plot_makers/"><span class="fa fa-instagram"></span></a>
+                        </div>
+                        <div class="col-3 search-top">
+                            <form action="{{ url('/pesquisar') }}" method="GET" class="search-top-form">
+                                <span class="icon fa fa-search"></span>
+                                <input type="text" name="query" id="s" placeholder="Pesquisar..." required>
+                                <button type="submit" style="display: none;"></button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
 
-        <main class="py-4">
+            <div class="container logo-wrap">
+                <div class="row pt-5">
+                    <div class="col-12 text-center">
+                        <a class="absolute-toggle d-block d-md-none" data-toggle="collapse" href="#navbarMenu" role="button" aria-expanded="false" aria-controls="navbarMenu"><span class="burger-lines"></span></a>
+                        <h1 class="site-logo">
+                            <a href="{{ url('/pagPrincip') }}"><img src="{{ asset('template/images/headerTcc__1_-removebg-preview.png') }}" alt=""></a>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <div class="container">
+                    <div class="collapse navbar-collapse" id="navbarMenu">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ url('/') }}">Página Principal</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="{{ url('/dicas') }}" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dicas de escrita</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Personagens</a>
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Capítulos</a>
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Estrutura</a>
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Desenvolvimento</a>
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Plots</a>
+                                    <a class="dropdown-item" href="{{ url('/dicas') }}">Outros </a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/about') }}">Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/perfil') }}">Perfil</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
             @yield('content')
         </main>
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-md-4">
+                        <h3>Sobre</h3>
+                        <p class="mb-4">
+                            <img src="{{ asset('template/images/logoTcc.png') }}" alt="Logo Plot Makers" class="img-fluid">
+                        </p>
+                        <p>Ajudando escritores iniciantes a começarem a trilhar seu caminho no mundo da escrita.</p>
+                    </div>
+                    <div class="col-md-6 ml-auto">
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-4">
+                                <div class="mb-5">
+                                    <h3>Links Rápidos</h3>
+                                    <ul class="list-unstyled">
+                                        <li><a href="{{ url('/about') }}">Sobre nós</a></li>
+                                        <li><a href="{{ url('/dicas') }}">Dicas de Escrita</a></li>
+                                        <li><a href="{{ url('/perfil') }}">Perfil</a></li>
+                                        <li><a href="{{ url('/pagPrincip') }}">Página Principal</a></li>
+                                    </ul>
+                                </div>
+                                <div class="mb-5">
+                                    <h3>Social</h3>
+                                    <ul class="list-unstyled footer-social">
+                                        <li><a href="https://twitter.com/PlotMakers"><span class="fa fa-twitter"></span> Twitter</a></li>
+                                        <li><a href="https://www.instagram.com/plot_makers/"><span class="fa fa-instagram"></span> Instagram</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p class="small">
+                            Copyright &copy;
+                            <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script> All Rights Reserved | This template is made with <i class="fa fa-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
+
+    <script src="{{ asset('template/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery-migrate-3.0.0.js') }}"></script>
+    <script src="{{ asset('template/js/popper.min.js') }}"></script>
+    <script src="{{ asset('template/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('template/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ asset('template/js/main.js') }}"></script>
 </body>
+
 </html>
